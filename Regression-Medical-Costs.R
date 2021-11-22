7fe35f348cffbcb5c726cd463c424df3bba7f812
Ian Skinner <ianskinneruh@gmail.com>
2021-11-22 14:31:08 -0600

    added distribution functions, created dummies, preprocessed variables

--- Regression-Medical-Costs.R
+++ Regression-Medical-Costs.R
@@ -1,13 +1,15 @@
 #Ian Skinner
 #2021-11-22
 #Objective: predict medical charges per person
+#Source: https://www.kaggle.com/mirichoi0218/insurance
 
 #setup===========================================================================================================================================================================
 rm(list = ls())
 options(scipen = 999)
+set.seed(808)
 
 #call pkgs
-pacman::p_load(tidyverse, janitor, scales, rio, dplyr, lubridate, gtsummary)
+pacman::p_load(tidyverse, janitor, scales, rio, dplyr, lubridate, gtsummary, caret)
 theme_set(theme_classic())
 
 #import data
@@ -21,7 +23,7 @@ medical = rio::import("insurance.csv") %>%
 slice_head(medical, n = 10)
 summary(medical)
 
-#na evaluation
+#na evaluatio
 nrow(anti_join(medical, na.omit(medical), by = "id"))
 
 #summary tables
@@ -37,7 +39,7 @@ medical %>%
   select(region, age, bmi, children, charges) %>% 
   tbl_summary(by = "region")
 
-#variable distributions
+#continuous variable distributions
 dist_fn = function(df, varname) {
   ggplot(data = df,
          aes(x = {{varname}})) +
@@ -50,3 +52,57 @@ dist_fn = function(df, varname) {
 dist_fn(medical, age)
 dist_fn(medical, bmi)
 dist_fn(medical, charges)
+
+#discrete and categorical variables
+disc_fn = function(df, varname) {
+  df %>% 
+    count({{varname}}) %>% 
+    mutate(pct = n / sum(n)) %>% 
+    ggplot(aes(x = {{varname}},
+               y = pct)) +
+    geom_col(width = .5) +
+    scale_y_continuous(labels = percent)
+}
+
+disc_fn(medical, sex)
+disc_fn(medical, smoker)
+disc_fn(medical, region)
+
+#recode children
+medical = medical %>% 
+  mutate(children = as.factor(children))
+
+#dummy varible creation
+dummy_vars = c("sex", "smoker", "region", "children")
+
+dummy_df = medical %>% select(all_of(dummy_vars))
+medical_df = medical %>% select(!all_of(dummy_vars))
+
+dummy_df = data.frame(predict(dummyVars(data = dummy_df, 
+                                        "~.", 
+                                        sep = "_"), 
+                              dummy_df))
+
+#join data back together
+medical = bind_cols(medical_df, dummy_df)
+
+#variables for preprocessing
+preprocess_vars = c("age", "bmi", "charges")
+
+preprocess_df = medical %>% select(all_of(preprocess_vars))
+medical_df = medical %>% select(!all_of(preprocess_vars))
+
+preprocess_df = data.frame(predict(preProcess(x = preprocess_df,
+                                              method = c("center", "scale")),
+                                   preprocess_df))
+
+#join data back together
+medical = bind_cols(medical_df, preprocess_df) %>% 
+  select(!id)
+
+#correlations
+cor_mtx = cor(medical)
+
+highly_correlated = findCorrelation(cor_mtx,
+                                    names = TRUE,
+                                    cutoff = 0.7)
